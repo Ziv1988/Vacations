6f10c8a79f6fdc61269b7e3e7535dec23d0e305d
Ziv1988 <zivsh88@gmail.com>
2019-11-12 08:45:25 +0200

    server is set for users first login

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/addUser', function (req, res) {
+  console.log(req.body.first_name),
+  console.log(req.body.last_name),
+  console.log(req.body.username),
+  console.log(req.body.password)
+})
+
+
 module.exports = router;
